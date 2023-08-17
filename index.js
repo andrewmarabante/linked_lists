d3c1f994152da2673b25e9a162c17d3d3fa27155
Andrew Marabante <andrewmarabante@gmail.com>
2023-08-16 17:46:18 -0700

    Created a LinkedList factory

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-alert('working')
+
+function linkedList()
+{
+    head = null;
+    next = null;
+    length = 0;
+    return{
+        head,
+        next,
+        length,
+        append(value)
+        {
+            console.log('working')
+        }
+    }
+}
+
+function node(value = null, next = null )
+{
+    return{
+        value, next
+    }
+}
+const linkedList1 = new linkedList();
+console.log(linkedList1.append());
