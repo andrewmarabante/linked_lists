58f8916d098b21893d3f99da9745f63b318bdf3a
Andrew Marabante <andrewmarabante@gmail.com>
2023-08-16 18:44:04 -0700

    Created an append function

--- index.js
+++ index.js
@@ -4,14 +4,36 @@ class linkedList
     constructor()
     {
         this.head = null;
+        this.tail = null;
         this.length = 0;
     }   
+
+    append(value)
+    {
+        const newNode = new Node(value, null);
+        if (this.tail.next === null)
+        {
+            this.tail.next = newNode
+        }
+        this.tail = newNode;
+        this.length++;
+        console.log('marker');
+        console.log(this)
+        if (this.head === null)
+        {
+            this.head = newNode;
+        }
+    }
 
     prepend(value)
     {
         const newNode = new Node(value, this.head);
         this.head = newNode;
         this.length++;
+        if (this.tail === null)
+        {
+            this.tail = newNode
+        }
     }
 
 }
@@ -27,5 +49,5 @@ class Node
 const linkedlist1 = new linkedList()
 linkedlist1.prepend(10);
 linkedlist1.prepend(20);
-linkedlist1.prepend();
-console.log(linkedlist1)
+linkedlist1.append(5);
+console.log(linkedlist1);
